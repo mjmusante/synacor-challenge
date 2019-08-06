80083bae14d9bc244f7e1403330f7773d950b634
Mark Musante <mark.musante@gmail.com>
2019-08-05 22:08:31 -0400

    initial tui code - alternate screen and q to quit

--- dbgr/src/main.rs
+++ dbgr/src/main.rs
@@ -1,12 +1,21 @@
+mod util;
+
 use std::io;
+
+use termion::event::Key;
 use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
+
 use tui::Terminal;
 use tui::backend::TermionBackend;
 use tui::widgets::{Widget, Block, Borders};
+
+use crate::util::event::{Event, Events};
 // use tui::layout::{Layout, Constraint, Direction};
 
-fn main() -> Result<(), io::Error> {
+fn main() -> Result<(), failure::Error> {
     let stdout = io::stdout().into_raw_mode()?;
+    let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     terminal.draw(|mut f| {
@@ -15,5 +24,23 @@ fn main() -> Result<(), io::Error> {
             .title("Block")
             .borders(Borders::ALL)
             .render(&mut f, size);
-    })
+    })?;
+
+    let events = Events::new();
+
+    loop {
+
+        match events.next()? {
+            Event::Input(key) => match key {
+                Key::Char('q') => {
+                    break;
+                },
+                _ => { },
+            },
+            _ => { },
+
+        }
+    }
+
+    Ok(())
 }
